f232b2294cacc7a209d9c06eea97dbc02bc6bb8a
ArtemDaemon <artem.daemon.official@gmail.com>
2024-01-06 15:24:23 +0700

    Fix bugs

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ def main():
     task_list = []
 
     tprint('To-Do List')
-    console.print('Welcome to the To-Do List!', style='italic')
+    console.print('Welcome to the To-Do List!\n', style='italic')
 
-    console.print('[red]1. [/red] Add new task')
-    console.print('[red]2. [/red] Show task list')
-    console.print('[red]3. [/red] Complete task')
-    console.print('[red]4. [/red] Delete task')
-    console.print('[red]5. [/red] Sort task list')
-    console.print('[red]6. [/red] Exit')
+    console.print('[red]1.[/red] Add new task')
+    console.print('[red]2.[/red] Show task list')
+    console.print('[red]3.[/red] Complete task')
+    console.print('[red]4.[/red] Delete task')
+    console.print('[red]5.[/red] Sort task list')
+    console.print('[red]6.[/red] Exit\n')
 
-    input_key = Prompt.ask('Please, choose menu item (1-6)',
-                           choices=['1', '2', '3', '4', '5', '6'], show_choices=False)
+    input_key = ''
 
     while input_key != EXIT_KEY:
+        input_key = Prompt.ask('Please, choose menu item (1-6)',
+                               choices=['1', '2', '3', '4', '5', '6'], show_choices=False)
+
         match input_key:
             case '1':
                 task_name = Prompt.ask('Enter a task name')
@@ -31,7 +33,8 @@ def main():
                     'name': task_name,
                     'is_done': False
                 })
-                console.print('f[green]The "{task}" task has been added[/green]', task_name)
+                console.print(f'The [green]"{task_name}"[/green] task has been added!\n')
+            # TODO: case '2'
 
     console.print('Thanks for using To-Do List!')
 
